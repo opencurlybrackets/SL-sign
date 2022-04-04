d55c901b8cda4f04cf95c9ada2571ffc7c507927
opencurlybrackets <Leonard.Haller@gmail.com>
2022-04-04 22:29:48 +0200

    220404 2301 changed env variables in heroku and in main.py changed "postgres://" to "postgresql://" line 46: DATABASE_URL2
    os.environ.get("DATABASE_URL2",  "sqlite:///blog.db")

--- main.py
+++ main.py
@@ -43,7 +43,8 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # #CONNECT TO DBDATABASE_URL
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")  #'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL2",  "sqlite:///blog.db")
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")  #'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
